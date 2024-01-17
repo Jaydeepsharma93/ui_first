af447257477cc21645944c80fa71e58eea341773
jaydeepsharma93 <sharmajaydeep@gmail.com>
2024-01-17 22:32:56 +0530

    17/01/2024

--- lib/main.dart
+++ lib/main.dart
@@ -101,227 +101,60 @@ class MyApp extends StatelessWidget {
       //   ),
       // ),
 
-      home: SafeArea(child:
-      Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.red,
 
-          title:const Text('Red & White ',style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w800,
-            letterSpacing: 1.1,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title:const Text('RichText  Example',
+              style:TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            centerTitle: true,
+
           ),
-          ),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: RichText(
-            text:const TextSpan(
-                children: <TextSpan>[
-                  TextSpan(text: '         G',style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-                  ),
-                  ),
-                  TextSpan(text: 'R',style: TextStyle(
-                    color: Colors.red,
-                    fontSize:40,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-                  )
-                  ),
-
-                  TextSpan(
-                      text: 'APHICS\n',style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-                  )
-                  ),
-                  TextSpan(text: '  FLUTT',style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    letterSpacing: 5,
-                  )
-                  ),
-                  TextSpan(text: 'E', style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'R\n', style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: '       AN',style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'D',style: TextStyle(
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'ROID\n',style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'DESIGN',style: TextStyle(
-                    color: Colors.amber,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: '&',style: TextStyle(
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 45,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'DEVELOP\n',style: TextStyle(
-                    color: Colors.amber,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: "         W",style: TextStyle(
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'EB\n',style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: '     FAS',style: TextStyle(
-                    color: Colors.yellow,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    letterSpacing: 6,
-
-                  )
-                  ),
-                  TextSpan(text: 'H',style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-
-                  )
-
-                  ),
-                  TextSpan(text: 'ION\n',style: TextStyle(
-                    color: Colors.yellow,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'ANIMAT',style: TextStyle(
-                    color: Colors.teal,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'I',style: TextStyle(
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'ON\n',style: TextStyle(
-                    color: Colors.teal,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: '          I',style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'T',style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'A-CS+\n',style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: '     GAM',style: TextStyle(
-                    color: Colors.amber,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    letterSpacing: 5,
-
-                  )
-                  ),
-                  TextSpan(text: 'E',style: TextStyle(
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                    letterSpacing: 5,
-                  )
-                  ),
-                ]
+          body:  Center(
+            child:RichText(
+              text:const TextSpan(
+                  children: [
+                    TextSpan(text: 'Single  ',
+                      style: TextStyle(
+                        color: Color(0xff2196F3),
+                        fontWeight: FontWeight.w900,
+                        fontSize: 30,
+                        fontFamily: "Google Sans",
+                      ),
+                    ),
+                    TextSpan(text: 'Line',style:
+                    TextStyle(
+                      color: Color(0xffFFFFFF),
+                      backgroundColor: Color(0xff009688),
+                      fontSize: 30,
+                      fontFamily: "Google Sans",
+                      fontWeight: FontWeight.w900,
+                    )
+                    ),
+                    TextSpan(text: '  Multiple',
+                        style: TextStyle(
+                          color: Color(0xffFF9800),
+                          fontSize: 30,
+                          fontFamily: 'RobotoMono',
+                          fontWeight: FontWeight.w900,
+                        )
+                    ),
+                    TextSpan(text: '  Style',
+                        style: TextStyle(
+                          color: Color(0xffE91D63),
+                          fontSize: 30,
+                          fontWeight: FontWeight.w900,
+                        )
+                    ),
+                  ]
+              ),
             ),
           ),
         ),
-      ),
       ),
     );
   }
